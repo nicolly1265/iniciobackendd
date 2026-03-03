9777007d9907e5f84995111add326f047ac5bc1c
nicolly1265 <nicolly.santos.ribeiro@escola.pr.gov.br>
2026-03-03 09:35:15 -0300

    Add files via upload

---
+++ array.js
@@ -0,0 +1,22 @@
+let frutas=["maça","banana","laranja"];
+console.log(frutas.length);
+console.log(frutas);
+frutas.push("mangas");
+console.log(frutas);
+console.log(frutas.length);
+frutas.pop();
+frutas.pop();
+frutas.pop();
+frutas.pop();
+console.log(frutas);
+console.log(frutas.length);
+frutas.push("uva");
+frutas.push("amora");
+frutas.push("mexerica","abacate","abacaxi");
+console.log(frutas);
+console.log("a fruta na posição 1",frutas[1]);
+console.log(frutas.length);
+console.log("o total de registros é de ",frutas.length+1,"frutas.");
+frutas.unshift("cereja")
+console.log(frutas)
+console.log(frutas.length)
